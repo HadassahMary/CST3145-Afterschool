c0fa859e90bc0379be31b6d9478b9806c4dc4ea4
HadassahMary <66474028+HadassahMary@users.noreply.github.com>
2022-04-13 18:40:56 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "https://hadassahmary.github.io/CST3145-Afterschool/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "CST3145-Afterschool" : "/",
 })
